4563b406050678ebbcba8c12a6c87e1a3ce9e184
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-27 12:33:53 +0200

    anyadir componente en js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  //anyadir el json de los objetos
+  lists = [{
+    "listId": "1",
+    "name": "Backlog",
+    "tasks": [{
+        "taskId": "1",
+        "text": "Aprender Express",
+        "completed": false,
+        "color": "tomato"
+    }, {
+        "taskId": "2",
+        "text": "Aprender JS",
+        "completed": true,
+        "color": "grey"
+    }, {
+        "taskId": "task-1527004083098-28",
+        "text": "asda",
+        "completed": false,
+        "color": "tomato"
+    }]
+ }, {
+    "listId": "2",
+    "name": "To Do",
+    "tasks": [{
+        "taskId": "3",
+        "text": "Aprender Node.js",
+        "completed": false,
+        "color": "tomato"
+    }, {
+        "taskId": "task-1527004086049-42",
+        "text": "asdas",
+        "completed": false,
+        "color": "tomato"
+    }]
+ }, {
+    "listId": "3",
+    "name": "Doing",
+    "tasks": [{
+        "taskId": "task-1527004088681-52",
+        "text": "asdasd",
+        "completed": false,
+        "color": "tomato"
+    }]
+ }]
 }
